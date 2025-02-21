ee060c6721e2cd3685373995c676b3f8338598c5
Redian Fikri <redianmf@gmail.com>
2025-02-21 15:31:16 +0700

    feat: add endpoints for token list source

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,11 +1,18 @@
 import axios from "axios";
 import { saveToFile } from "./saveToFile";
 
+const odosTokensEndpoint = "https://api.odos.xyz/info/tokens/137";
+const deBridgeTokensEndpoint =
+  "https://dln.debridge.finance/v1.0/token-list?chainId=137";
+const lifiTokensEndpoint = "https://li.quest/v1/tokens?chainTypes=EVM";
+const hashportTokensEndpoint =
+  "https://mainnet.api.hashport.network/api/v1/assets";
+
 const fetcher = async () => {
-  const res = await axios.get("https://api.odos.xyz/info/tokens/137");
+  const res = await axios.get(lifiTokensEndpoint);
   const data = JSON.stringify(res.data);
 
-  await saveToFile("./src/data", "testTokens.json", data);
+  await saveToFile("./src/data", "lifiTokens.json", data);
 };
 
 fetcher().catch((e) => console.log(e));
